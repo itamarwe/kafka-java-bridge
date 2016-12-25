a2551bb2ff19e584a7270611d9a9486eca15b798
Itamar Weiss <weiss.itamar@gmail.com>
2016-12-25 10:19:03 +0200

    fixing reference

--- java/src/com/liveperson/kafka/consumer/MultiThreadHLConsumer.java
+++ java/src/com/liveperson/kafka/consumer/MultiThreadHLConsumer.java
@@ -70,7 +70,7 @@ public class MultiThreadHLConsumer {
     public void stop(){
         if (consumer != null) {
             consumer.shutdown();
-            this.resume();
+            resume();
         }
 
         if (executor != null) {
