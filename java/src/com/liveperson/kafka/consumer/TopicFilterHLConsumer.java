78b075e37767b6209da5253abb2aaa106079ea6f
Itamar Weiss <weiss.itamar@gmail.com>
2017-01-09 22:22:55 +0200

    Fix

--- java/src/com/liveperson/kafka/consumer/TopicFilterHLConsumer.java
+++ java/src/com/liveperson/kafka/consumer/TopicFilterHLConsumer.java
@@ -55,7 +55,7 @@ public class TopicFilterHLConsumer {
 
     public void start(){
         consumer = Consumer.createJavaConsumerConnector(new ConsumerConfig(properties));
-        List<KafkaStream<byte[], byte[]>> streams  = consumer.createMessageStreamsByFilter(this.topicFilter, this.threadCount);
+        List<KafkaStream<byte[], byte[]>> streams  = consumer.createMessageStreamsByFilter(this.topicFilter, threadCount);
 
         executor = Executors.newFixedThreadPool(threadCount);
 
